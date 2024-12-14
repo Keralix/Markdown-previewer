b3bb37417d967de92b7b256cc56d8a28d37a4d4a
Tomislav Bračić <tbracic43@gmail.com>
2024-12-14 21:49:27 +0100

    almost done need to fix few bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import './App.css'
 import { useSelector,useDispatch } from 'react-redux'
-import { change,selectText } from './features/markdownSlice'
+import { change,getText,selectText } from './features/markdownSlice'
 //react
 function App() {
   const markdownText = useSelector(selectText)
+  const markdownFinish =  useSelector(getText)
   const dispatch = useDispatch();
   return (
     <>
-      <div id="editorWrapper" className='container py-5'>  
+      <div id="editorWrapper" className='container py-5 '>  
         <div className="card">
           <h5 className="card-header">Editor</h5>  
-          <textarea id="editor" className='card-body form-control 'onChange={e=>dispatch(change(e.target.value))}>{markdownText}</textarea>
+          <textarea id="editor" className='card-body form-control ' rows="8" onChange={e=>dispatch(change(e.target.value))}>{markdownText}</textarea>
         </div>
       </div>
       
-      <div id="previewWrapper" className='container'>
+      <div id="previewWrapper" className='container '>
         <div className='card'>
         <h5 className='card-header'>Preview</h5>
-        <div id="preview" className='card-body'>{markdownText}</div>
+        <div id="preview" className='card-body' dangerouslySetInnerHTML={{__html: markdownFinish}}></div>
         </div>
       </div>
     </>
--- src/features/markdownSlice.js
+++ src/features/markdownSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { marked } from "marked";
 
 export const markdownSlice = createSlice({
   name: "markdown",
   initialState: {
-    text: "isus223",
+    text: "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n```\n// this is multi-line code:\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)",
   },
   reducers: {
     change: (state, action) => {
@@ -14,4 +15,5 @@ export const markdownSlice = createSlice({
 
 export const { change } = markdownSlice.actions;
 export const selectText = (state) => state.markdown.text;
+export const getText = (state) => marked.parse(state.markdown.text);
 export default markdownSlice.reducer;
